9db40ffaf2e95ca7da1799065229ea791ea90059
Olena Avdiunina <elena.avdiunina@gmail.com>
2021-10-19 13:33:29 +0300

    cart

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# shoping-cart
+Test task
---
+++ src/data.js
@@ -0,0 +1,32 @@
+// import img1 from './img/img1.png';
+// import img2 from './img/img2.png';
+// import img3 from './img/img3.png';
+import img from './img/fruits.png';
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img,
+            title: "Banana",
+            price: 10,
+            desc: "",
+        },
+        {
+            id: 2,
+            img: img,
+            title: "Apple",
+            price: 8,
+            desc: "",
+        },
+        {
+            id: 3,
+            img: img,
+            title: "Papaya",
+            price: 10,
+            desc: "",
+        }
+    ],
+};
+
+export default data;
